d9ea09f30bdeac2480fd32f24e726f964eee58ea
Michelangelo-Ancora-Impora <58369436+mmertylmz@users.noreply.github.com>
2022-07-15 17:16:36 +0300

    APIs

---
+++ ApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,79 @@
+﻿using ApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values = c.Employees.ToList();
+            return Ok(values); //Ok, API isteği başarılıysa geri döndürecek status code'dur.
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            using var c = new Context();
+            c.Add(employee);
+            c.SaveChanges();
+            return Ok();
+            
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            using var c = new Context();
+            var employee=c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                c.Remove(employee);
+                c.SaveChanges();
+                return Ok();
+            }
+            
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Find<Employee>(employee.ID);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                emp.Name = employee.Name;
+                c.Update(emp);
+                c.SaveChanges();
+                return Ok();
+            }
+        }
+    }
+
+    
+}
--- Core Web App Tutorial/Areas/Admin/Controllers/WriterController.cs
+++ Core Web App Tutorial/Areas/Admin/Controllers/WriterController.cs
@@ -18,7 +18,38 @@ namespace Core_Web_App_Tutorial.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
-        public static List<WriterClass> writers = new List<WriterClass> //Statik
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.id.Equals(writerid));
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult WriterAdd(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+            
+        }
+
+        public IActionResult WriterDelete(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.id.Equals(id));
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
+        public IActionResult WriterUpdate(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.id.Equals(w.id));
+            writer.name = w.name;
+            var jsonWriter = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriter);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass> //Static
         {
             new WriterClass
             {
